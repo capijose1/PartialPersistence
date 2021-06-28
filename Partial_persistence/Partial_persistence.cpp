b1e778d7961c931dd55ea8a267f7328ee21cebed
José Emmanuel Tito Acrota <jose.tito.acrota@outlook.com>
2021-06-28 05:28:20 -0500

    Todo funciona

---
+++ Partial_persistence/Partial_persistence.cpp
@@ -0,0 +1,51 @@
+﻿// Partial_persistence.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Partial_persistence.h"
+
+
+
+int main()
+{
+    vector<int> lista;
+    for (int i = 10; i < 20; i++) {
+        lista.push_back(i);
+    }
+    /// Aqui creamos una lista para poder insertar un vector a la cabeza
+    LPP<int> a(lista); //Inicializamos la lista Partial Persistence con un vector lista
+    for (int i = 0; i < 10; i++) {
+        a.insert(i);
+    }//Insertamos elementos unicos a nuestra lista
+    for (Nodo<int>* i = a.head; i != nullptr; i = i->next) {
+        cout << i->valor[0] << "_tam(" << i->valor.size() << ")->"; //Imprimimos los valores iniciales de nuestra lista con su respectivo tamaño
+    }
+    cout << endl;
+    cout << "Numero de versiones" << a.head->PP.size() << endl;
+    a.insertar(a.head, 3, 8); //Insertamos al nodo cabeza los datos a modificar y sus respectivas modificaciones
+    cout << "Numero de versiones con una modificaciones con insert: " << a.head->PP.size() << " Se modifico el puntero cabeza, siendo el campo 3 con el numero 8" << endl;
+    a.insertar(a.head, 4, 40);
+    cout << "Numero de versiones con dos modificaciones con insert: " << a.head->PP.size() << " Se modifico el puntero cabeza, siendo el campo 4 con el numero 5" << endl;
+    cout << endl << endl << "Imprimimos la version 0 o prima de la cabeza " << endl;
+    for (int i = 0; i < a.head->valor.size(); i++) {
+        cout << a.head->valor[i] << "->";
+    }
+    cout << endl << endl << "Imprimimos la version 1" << endl << "Para esto buscamos en la posicion 0 ya que es la que tiene la lista,la version 1,el campo 3" << endl;
+    Nodo<int>* temporal1 = a.buscar(0, 1, 3);
+    for (int i = 0; i < a.head->valor.size(); i++) {
+        cout << temporal1->valor[i] << "->";
+    }
+    cout << endl << endl << "Imprimimos la version 2" << endl << "Para esto buscamos en la posicion 0 ya que es la que tiene la lista,la version 2,el campo 3" << endl;
+    temporal1 = a.buscar(0, 2, 4);
+    for (int i = 0; i < a.head->valor.size(); i++) {
+        cout << temporal1->valor[i] << "->";
+    }
+    cout << endl << endl << endl;
+    a.insertar(a.head, 4, a.head->next->next->next);
+    for (Nodo<int>* i = a.head; i != nullptr; i = i->next) {
+        cout << i->valor[0] << "->"; //Imprimimos los valores iniciales de nuestra lista con su respectivo tamaño
+    }
+    cout << endl;
+    cout << "el valor actual de head es : " << a.head->valor[0] << endl;
+    cout << "El siguiente despues de head normalmente es: " << a.head->next->valor[0] << endl;
+    cout << "Con el insert nodo es :" << a.head->PP.back()->next->valor[0] << endl;
+}
---
+++ Partial_persistence/Partial_persistence.h
@@ -0,0 +1,108 @@
+﻿// Partial_persistence.h: archivo de inclusión para archivos de inclusión estándar del sistema,
+// o archivos de inclusión específicos de un proyecto.
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <utility>
+using namespace std;
+template <typename T>
+struct Nodo
+{
+    vector<T> valor;
+    Nodo <T>* next;
+    vector<Nodo <T>*> PP;
+    Nodo() {
+        this->next = nullptr;
+    }
+    Nodo(vector<T> valor, Nodo* next = nullptr) {
+        this->valor = valor;
+        this->next = next;
+    }
+    Nodo(T valor, Nodo* next = nullptr) {
+        this->valor.push_back(valor);
+        this->next = next;
+    }
+    void insertar(T valor, Nodo* next = nullptr) {
+        this->valor.push_back(valor);
+        this->next = next;
+    }
+
+};
+template <typename T>
+struct LPP {
+    Nodo<int>* head = new Nodo<T>;
+    Nodo<int>* pos = head;
+    LPP(T valor) {
+        head = new Nodo<T>(valor);
+        pos = head;
+    }
+    LPP(vector<T> valor) {
+        head = new Nodo<T>(valor);
+        pos = head;
+    }
+    void insert(T valor) {
+        pos->next = new Nodo<T>(valor);
+        pos = pos->next;
+    }
+    void insert(vector<T> valor) {
+        pos->next = new Nodo<T>(valor);
+        pos = pos->next;
+    }
+    void write(Nodo<T>* ptr, T valor) {
+        if (ptr->valor.size() != 0) {
+            Nodo<T>* tmp = new Nodo<T>(ptr->valor);
+            tmp->insertar(valor);
+            tmp->next = ptr->next;
+            ptr->PP.push_back(tmp);
+        }
+
+    }
+    void insertar(Nodo<T>* ptr, int campo, T val) {
+        Nodo<T>* tmp;
+        if (ptr->PP.size() != 0) {
+            tmp = new Nodo<T>(ptr->PP.back()->valor);
+        }
+        else {
+            tmp = new Nodo<T>(ptr->valor);
+        }
+        tmp->valor[campo] = val;
+        tmp->next = ptr->next;
+        ptr->PP.push_back(tmp);
+
+    }
+    void insertar(Nodo<T>* ptr, int campo, Nodo<T>* val) {
+        Nodo<T>* tmp = new Nodo<T>(ptr->PP.back()->valor);
+        tmp->next = val;
+        ptr->PP.push_back(tmp);
+    }
+    Nodo<T>* buscar(int pos, int version, int campo) {
+        Nodo<T>* tmp = head;
+        for (int i = 0; i < pos; i++) {
+            tmp = tmp->next;
+        }
+        int tam_ver = 1 + tmp->PP.size();
+        if (version > tam_ver) {
+            cout << "No se puede hacer esta operación , no existe la versión" << endl;
+        }
+        else {
+            if (version == 0) {
+                if (tmp->valor.size() > campo) {
+                    cout << "Se encontro el elemento: " << tmp->valor[campo] << endl;
+                    return tmp;
+                }
+            }
+            else {
+                if (tmp->PP[tam_ver - 1 - version]->valor.size() > campo) {
+                    cout << "Se encontro el elemento: " << tmp->PP[version - 1]->valor[campo] << endl;
+                    cout << "Version: " << version - 1 << endl;
+                    return tmp->PP[version - 1];
+                }
+            }
+        }
+
+    }
+};
+
+// TODO: Haga referencia aquí a los encabezados adicionales que el programa requiere.
